a03ba6922779d16c278dd453a7e3b5f9c8411fae
rodrigoalmeidaj <rjalmeiida@gmail.com>
2022-12-14 13:23:20 -0300

    Novos inserts no banco de dados

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 insert into tb_category (name) values ('Brinquedos');
 insert into tb_category (name) values ('Livros');
 insert into tb_category (name) values ('Eletrodomesticos');
+insert into tb_category (name) values ('Móveis');
+insert into tb_category (name) values ('Informática');
