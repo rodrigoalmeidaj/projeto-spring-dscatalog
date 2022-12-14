20fc1009751e352b06e39f791ebad115d65a42b7
rodrigoalmeidaj <rjalmeiida@gmail.com>
2022-12-14 20:04:55 -0300

    Rota POST Category(insertCategory)

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -40,4 +40,13 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 	
+	@Transactional
+	public CategoryDTO insertCategory(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+		
+	}
+	
 }
