dda54ccdc1d2eeea5dd916936bdc4e17862f7ee1
rodrigoalmeidaj <rjalmeiida@gmail.com>
2022-12-14 13:46:29 -0300

    Nova rota findbyid na categorys

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.services;
 
 //import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,13 @@ public class CategoryService {
 			//listDto.add(new CategoryDTO(cat));
 		//}
 		//return listDto;
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 	
 }
